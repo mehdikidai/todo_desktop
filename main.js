71d2253ab2783fd2cfe4ed061e1e4b70d4ca8095
mehdi_kidai <mehdikidai@gmail.com>
2024-05-17 23:24:42 +0100

    c4

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const createWindow = () => {
     const win = new BrowserWindow({
         width: 1000,
         minWidth:900,
-        height: 800,
-        minHeight: 800,
+        height: 700,
+        minHeight: 700,
         webPreferences: {
             preload: path.join(__dirname, "preload.js"),
         },
